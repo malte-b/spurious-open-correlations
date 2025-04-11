d3e5b831fbf5eef3ab43c622812fc5600501c915
malte-b <malte.barth01@gmail.com>
2025-04-11 10:01:44 +0200

    refactor: file org

--- berlin-millionaire/components/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-[60vh]">
-      <div className="w-16 h-16 border-4 border-berlin-gray-300 border-t-berlin-red rounded-full animate-spin"></div>
-      <p className="mt-4 text-xl text-berlin-black uppercase">Fragen werden geladen...</p>
-    </div>
-  )
-}
